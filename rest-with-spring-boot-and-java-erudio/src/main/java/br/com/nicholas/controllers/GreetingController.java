3d1a54d50c58462412c1c03cf1a6a2fc818f020b
nickdiegao <nickdiegao@gmail.com>
2025-08-27 20:57:16 -0300

    First steps in Java with Spring Boot 3.4.9

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/nicholas/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package br.com.nicholas.controllers;
+
+import br.com.nicholas.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    // http://localhost:8080/greeting?name=Nicholas
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "Word")
+            String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
